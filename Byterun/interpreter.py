388a0f18f04345d64a1a69cdfc8a17c0ae2382e6
ChengBang Zhou <zhouchengbang@gmail.com>
2018-04-25 19:18:48 +0800

    add two value

--- Byterun/interpreter.py
+++ Byterun/interpreter.py
@@ -1,4 +1,21 @@
 
+
+# what_to_execute = {
+#     "instructions": [("LOAD_VALUE", 0),  # the first number
+#                      ("LOAD_VALUE", 1),  # the second number
+#                      ("ADD_TWO_VALUES", None),
+#                      ("PRINT_ANSWER", None)],
+#     "numbers": [7, 5] }
+
+what_to_execute = {
+        "instructions": [("LOAD_VALUE", 0),
+                         ("LOAD_VALUE", 1),
+                         ("ADD_TWO_VALUES", None),
+                         ("LOAD_VALUE", 2),
+                         ("ADD_TWO_VALUES", None),
+                         ("PRINT_ANSWER", None)],
+        "numbers": [7, 5, 8] }
+
 class Interpreter:
     def __init__(self):
         self.stack = []
@@ -15,3 +32,21 @@ class Interpreter:
         second_num = self.stack.pop()
         total = first_num + second_num
         self.stack.append(total)
+
+    def run_code(self, what_to_execute):
+        instructions = what_to_execute["instructions"]
+        numbers = what_to_execute["numbers"]
+        for each_step in instructions:
+            instruction, argument = each_step
+            if instruction == "LOAD_VALUE":
+                number = numbers[argument]
+                self.LOAD_VALUE(number)
+            elif instruction == "ADD_TWO_VALUES":
+                self.ADD_TWO_VALUES()
+            elif instruction == "PRINT_ANSWER":
+                self.PRINT_ANSWER()
+
+
+if __name__ == '__main__':
+    interpreter = Interpreter()
+    interpreter.run_code(what_to_execute)
